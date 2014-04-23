bc54413874b28ccb521b3cc7520bf0449eaf9a3a
nodaguti <nodaguti@gmail.com>
2014-04-23 12:54:45 +0900

    自分の環境で動かなかったのを修正

--- macnotify.rb
+++ macnotify.rb
@@ -1,22 +1,18 @@
-require "terminal-notifier"
+require '/Library/Ruby/Gems/2.0.0/gems/terminal-notifier-1.6.0/lib/terminal-notifier.rb'
 
-Plugin.create(:macnotify) do
-  on_popup_notify do |user, text, &stop|
-    text = if text.is_a? Message
-      text.to_show
-    else
-      '' unless test.is_a? String
+Plugin.create :macnotify do
+    on_appear do |ms|
+        ms.each do |m|
+            text = m.message.to_show
+            user = "mikumiku"
+            user = "@#{m.message.user[:idname]} (#{m.message.user[:name]})" if m.message.user
+
+            text = text.encode(Encoding::UTF_8)
+            user = user.encode(Encoding::UTF_8)
+
+            if text.valid_encoding? && user.valid_encoding?
+                TerminalNotifier.notify(text, :title=>user, :sender=>'org.macosforge.xquartz.X11')
+            end
+        end
     end
-    
-    u = "mikumiku"
-    u = "@#{user[:idname]} (#{user[:name]})" if user
-
-    text = text.encode(Encoding::UTF_8)
-    u = u.encode(Encoding::UTF_8)
-
-    if text.valid_encoding? && u.valid_encoding?
-      TerminalNotifier.notify(text, :title=>u, :sender=>'org.macosforge.xquartz.X11')
-    end
-    stop.call
-  end
 end
